08dc74672eb8a85e7f95bd4baac5b29e7d1a7fce
aditya-pat <f20230721@pilani.bits-pilani.ac.in>
2026-01-09 18:45:09 +0530

    tbs added

--- labs/lab01/tb/tb_4bit.v
+++ labs/lab01/tb/tb_4bit.v
@@ -1,9 +1,12 @@
 module tb;
-  reg a, b, sel;
-  wire y;
+  reg [3:0]a; 
+  reg [3:0]b;
+  reg cin;
+  wire [3:0]sum;
+  wire cout;
   string vcd_file;
 
-  mux2 dut(.a(a),.b(b),.sel(sel),.y(y));
+  bit4add dut(.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
 
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file))
@@ -13,11 +16,14 @@ module tb;
 
     $dumpvars(0, dut);
 
-    a=0; b=0; sel=0; #10;
-    a=1; b=0; sel=0; #10;
-    a=0; b=1; sel=1; #10;
-    a=1; b=1; sel=1; #10;
+    a=4'H0; b=4'H0; cin=0; #10;
+    a=4'H6; b=4'HB; cin=1; #10;
+    a=4'HF; b=4'HF; cin=1; #10;
+    a=4'H0; b=4'H1; cin=0; #10;
+    a=4'HA; b=4'H9; cin=0; #10;
+    a=4'H4; b=4'H3; cin=1; #10;
 
     $finish;
   end
 endmodule
+
--- labs/lab01/tb/tb_4bit_addsub.v
+++ labs/lab01/tb/tb_4bit_addsub.v
@@ -1,9 +1,12 @@
 module tb;
-  reg a, b, sel;
-  wire y;
+  reg [3:0]a;
+  reg [3:0]b;
+  reg cin,m;
+  wire [3:0]sum;
+  wire cout;
   string vcd_file;
 
-  mux2 dut(.a(a),.b(b),.sel(sel),.y(y));
+  bit4addsub dut(.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout),.m(m));
 
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file))
@@ -13,11 +16,19 @@ module tb;
 
     $dumpvars(0, dut);
 
-    a=0; b=0; sel=0; #10;
-    a=1; b=0; sel=0; #10;
-    a=0; b=1; sel=1; #10;
-    a=1; b=1; sel=1; #10;
+    a=4'H4; b=4'H0; cin=0; m=0;#10;
+    a=4'H8; b=4'HB; cin=1; m=0;#10;
+    a=4'HD; b=4'HF; cin=1; m=0;#10;
+    a=4'H3; b=4'H1; cin=0; m=0;#10;
+    a=4'H0; b=4'H9; cin=0; m=0;#10;
+    a=4'HF; b=4'H0; cin=1; m=0;#10;
+
+    a=4'H4; b=4'H0; cin=0; m=1;#10;
+    a=4'H8; b=4'HB; cin=1; m=1;#10;
+    a=4'HD; b=4'HF; cin=1; m=1;#10;
+    a=4'H3; b=4'H1; cin=0; m=1;#10;
+    a=4'H0; b=4'H9; cin=0; m=1;#10;
+    a=4'HF; b=4'H0; cin=1; m=1;#10;
 
     $finish;
   end
-endmodule
